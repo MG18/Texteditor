ed31aab3257da9b482612c5c827a719ed1fcc3e4
Mehmet Gül <guelmeh1@students.zhaw.ch>
2024-12-01 17:38:11 +0100

    Commandword ENUM gemappt für den passenden String

--- src/CommandWord.java
+++ src/CommandWord.java
@@ -1,3 +1,27 @@
+import java.util.HashMap;
+
 public enum CommandWord {
-    ADD, DEL, FORMAT_FIX, FORMAT_RAW, INDEX, REPLACE, DUMMY, PRINT, EXIT
+    ADD("add"), DEL("del"), FORMAT_FIX("format fix"), FORMAT_RAW("foramt raw"), INDEX("index"), REPLACE("replace"), DUMMY("dummy"), PRINT("print"), EXIT("exit");
+
+    String command;
+    private static final HashMap<String,CommandWord> ENUM_MAP;
+
+    CommandWord(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return this.command;
+    }
+
+    static {
+        ENUM_MAP = new HashMap<String, CommandWord>();
+        for(CommandWord cw : CommandWord.values()) {
+            ENUM_MAP.put(cw.getCommand(), cw);
+        }
+    }
+
+    public static CommandWord get (String name) {
+        return ENUM_MAP.get(name.toLowerCase());
+    }
 }
